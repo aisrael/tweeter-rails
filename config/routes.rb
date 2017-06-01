e93ba5a33d0e43a718553c6c234de9a176343afa
Alistair A. Israel <aisrael@gmail.com>
2017-06-01 08:01:18 +0800

    Added readiness probe

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+
+  # GCP/Kubernetes service health check path
+  get '/healthz', to: proc { [200, {}, ['']] }
+
   resources :tweets
 
   root to: 'tweets#index'
